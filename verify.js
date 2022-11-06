6d5e17b7d6728863f8faa230e778391632993760
dimiourlab <103715121+dimiourlab@users.noreply.github.com>
2022-11-06 09:42:12 +0530

    Add files via upload

---
+++ verify.js
@@ -0,0 +1,26 @@
+let attempt = 3; 
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+var password = document.getElementById("password").value;
+if ( username == "kush.kundalia" && password == "kushk17"|| username=="dhyan.thakkar"&&password=="animeguy2000"||username == "tirth.khunt" && password == "tirthk92"||username == "vasu.rakholiya" && password=="vasur18"||username=="yuvraj.dadhaniya"&&password=="yuvrajd02"||username=="krish.kotadiya"&&password=="krishk10"||username=="tanya.punjabi"&&password=="tanyap88"||username=="saloni.rawal"&&password=="salonir7"){
+
+window.location = "studio.publication.html"; 
+return false;
+}
+else{
+attempt --;
+alert("Please Re-Check Your Credentials");
+
+
+    
+ if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+alert ("Your login rights have been suspended");
+
+return false;
+}
+}
+}
